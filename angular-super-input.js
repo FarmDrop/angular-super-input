7a6ba63928b8ea6a60d1929533e9e6e54953d499
Myles Cowper-Coles <mylescc@gmail.com>
2014-01-16 11:43:50 +0000

    using the model for ID as it must always be unique, and sorting out blur validity

--- angular-super-input.js
+++ angular-super-input.js
@@ -16,7 +16,7 @@ angular.module('superInput')
                     pre: function(scope,element,attrs){
                         scope.data ={
                             name: attrs.name,
-                            id: attrs.name.replace(' ',''),
+                            id: attrs.model.replace(/\./g,''),
                             type: attrs.type || 'text',
                             required: attrs.required || 'false',
                             minlength: attrs.minlength || 0
@@ -28,13 +28,13 @@ angular.module('superInput')
                         scope.form.$removeControl(ctrl); // Removes the current controller
                         input.attr('name',scope.data.id); // Attaches your name to the input
                         ctrl.$name = scope.data.id; // Changes the name of the input in the ctrl
-                        ctrl.$setValidity('visited',true); // Says there is an error with 'visited' - i.e. input has not been visited
+                        ctrl.$setValidity('visited',false); // Says there is an error with 'visited' - i.e. input has not been visited
                         scope.form.$addControl(ctrl); // Adds the controller back to the from
 
 
                         // On blur of the input, say that there is not an error with 'visited' - i.e. that the input has been visited
                         input.bind('blur',function(){
-                            ctrl.$setValidity('visited',false);
+                            ctrl.$setValidity('visited',true);
                             scope.$apply()
                         });
                     }
